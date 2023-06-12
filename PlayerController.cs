1a7532d128a7b827f1ca6b654c2c7ca2a815e9ad
Lorin Sinzig <132986698+lorinsinzig@users.noreply.github.com>
2023-06-12 14:42:06 +0200

    Added coyote time and jump buffering to the script.

--- PlayerController.cs
+++ PlayerController.cs
@@ -7,9 +7,13 @@ public class PlayerController : MonoBehaviour
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
     public float variableJumpHeightMultiplier = 0.5f; // Adjust this value to control the jump height
+    public float coyoteTime = 0.1f; // Adjust this value to control the coyote time
+    public float jumpBufferTime = 0.1f; // Adjust this value to control the jump buffer time
 
     private Rigidbody2D rb;
     private bool isGrounded;
+    private float coyoteTimer;
+    private float jumpBufferTimer;
 
     private void Start()
     {
@@ -24,21 +28,44 @@ public class PlayerController : MonoBehaviour
         // Check if the player is grounded
         isGrounded = Physics2D.OverlapCircle(groundCheckPosition, groundCheckRadius, groundLayer);
 
+        // Update the coyote timer
+        if (isGrounded)
+        {
+            coyoteTimer = coyoteTime;
+        }
+        else
+        {
+            coyoteTimer -= Time.deltaTime;
+        }
+
+        // Update the jump buffer timer
+        if (jumpBufferTimer > 0)
+        {
+            jumpBufferTimer -= Time.deltaTime;
+        }
+
         float moveX = Input.GetAxis("Horizontal");
 
         // Move the character horizontally
         rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
 
-        // Jump if the player is grounded and the Jump button is pressed
-        if (isGrounded && Input.GetButtonDown("Jump"))
+        // Jump if the player is grounded, within the coyote time, or within the jump buffer time
+        if ((isGrounded || coyoteTimer > 0 || jumpBufferTimer > 0) && Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpBufferTimer = 0;
         }
 
         // Apply variable jump height if the jump button is released early
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * variableJumpHeightMultiplier);
+        }
+
+        // Start jump buffer if the jump button is pressed within the buffer time
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpBufferTimer = jumpBufferTime;
         }
     }
 
