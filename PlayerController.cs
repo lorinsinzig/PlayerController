2c87121035960c7d261d067af6901a4edc075907
Lorin Sinzig <132986698+lorinsinzig@users.noreply.github.com>
2023-06-12 14:40:15 +0200

    Added Variable Height Jump to the script

--- PlayerController.cs
+++ PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 5f;
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
+    public float variableJumpHeightMultiplier = 0.5f; // Adjust this value to control the jump height
 
     private Rigidbody2D rb;
     private bool isGrounded;
@@ -31,7 +32,13 @@ public class PlayerController : MonoBehaviour
         // Jump if the player is grounded and the Jump button is pressed
         if (isGrounded && Input.GetButtonDown("Jump"))
         {
-            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        // Apply variable jump height if the jump button is released early
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * variableJumpHeightMultiplier);
         }
     }
 
